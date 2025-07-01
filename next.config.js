def9a592252ec36cb7794a2222d7cde36a87a9da
devzota <khamphadn43@gmail.com>
2025-07-01 22:26:46 +0700

    up

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@notionhq/client"],
+  },
+}
+
+module.exports = nextConfig
